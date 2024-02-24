243832482bef126c7a65dcaa092ffd39011559c8
Jhon Edinson Muñoz Riaños <je.munozr1@uniandes.edu.co>
2024-02-24 13:42:23 -0500

    Ajusta recovery logica

--- recovery-payment/api.py
+++ recovery-payment/api.py
@@ -24,6 +24,8 @@ hosts = [(host, port)]
 queue_name_one = os.environ.get('QUEUE_NAME_ONE', 'one')
 queue_name_two = os.environ.get('QUEUE_NAME_TWO', 'two')
 
+conn = stomp.Connection(host_and_ports=hosts)
+
 bd_path = os.environ.get('BD_PATH', 'sqlite:///recovery.db')
 
 # Database configuration
@@ -91,21 +93,20 @@ class PaymentListener(stomp.ConnectionListener):
                 is_retry=message_translated.get('is_retry', False),
                 processed=False,
             )
-            db.session.add(new_payment)
-            db.session.commit()
+            #db.session.add(new_payment)
+            #db.session.commit()
+            process_payments(new_payment)
             print('NOTIFY PENDING PAYMENT RECEIVED -> {%s:%s:%s}' % (
                 new_payment.id, new_payment.gateway, new_payment.value))
 
 
 def active_mq_listener():
-    conn = stomp.Connection(host_and_ports=hosts)
     conn.set_listener('', PaymentListener())
-    # conn.start()
     conn.connect('admin', 'admin', wait=True)
-    conn.subscribe(destination=queue_name_one, id=1, ack='auto')
-    conn.subscribe(destination=queue_name_two, id=2, ack='auto')
-    print(f"Subscribed to ActiveMQ queue: {queue_name_one}")
-    print(f"Subscribed to ActiveMQ queue: {queue_name_two}")
+    #conn.subscribe(destination=queue_name_one, id=1, ack='auto')
+    #conn.subscribe(destination=queue_name_two, id=2, ack='auto')
+    #print(f"Subscribed to ActiveMQ queue: {queue_name_one}")
+    #print(f"Subscribed to ActiveMQ queue: {queue_name_two}")
 
     while True:
         time.sleep(1)  # Keep the thread alive
@@ -130,8 +131,9 @@ def topic_stream():
                     current_payment_gateway_status['payment_gate_way_one'] = healthStatus['payment_gate_way_one']
                 elif 'payment_gate_way_two' in healthStatus:
                     current_payment_gateway_status['payment_gate_way_two'] = healthStatus['payment_gate_way_two']
-                    # Reprocess payment
-                process_payments()
+                
+                handle_subscribe()
+                
 
 
 def mapper_to_gateway(payment):
@@ -142,26 +144,48 @@ def mapper_to_gateway(payment):
         'is_retry': payment.is_retry,
     }
 
-
-def process_payments():
+def handle_subscribe():
     if current_payment_gateway_status['payment_gate_way_one'] == 'up':
-        unproccesed_payment_one = Payment.query.filter(Payment.gateway == 'one', Payment.processed == False).all()
-        for payment in unproccesed_payment_one:
-            payment.is_retry = True
-            response = requests.post(f"{hostOne}/execute_payment", json=payment_schema.dump(mapper_to_gateway(payment)))
-            if response.status_code == 200:
-                payment.processed = True
-                db.session.commit()
-                print('NOTIFY PAYMENT_GATEWAY_ONE PROCESSED -> {%s:%s}' % (payment.id, payment.value))
+        conn.subscribe(destination=queue_name_one, id=1, ack='auto')
+        print(f"Subscribed to ActiveMQ queue: {queue_name_one}")
+    elif current_payment_gateway_status['payment_gate_way_one'] == 'down':
+        try:
+            conn.unsubscribe(id=1)
+            print(f"Unsubscribed from ActiveMQ queue: {queue_name_one}")
+        except NameError:
+            print(f"Unknown subscription to ActiveMQ queue: {queue_name_one}")
+        
     if current_payment_gateway_status['payment_gate_way_two'] == 'up':
-        unproccesed_payment_two = Payment.query.filter(Payment.gateway == 'two', Payment.processed == False).all()
-        for payment in unproccesed_payment_two:
-            payment.is_retry = True
-            response = requests.post(f"{hostTwo}/execute_payment", json=payment_schema.dump(mapper_to_gateway(payment)))
-            if response.status_code == 200:
-                payment.processed = True
-                db.session.commit()
-                print('NOTIFY PAYMENT_GATEWAY_TWO PROCESSED -> {%s:%s}' % (payment.id, payment.value))
+        conn.subscribe(destination=queue_name_two, id=2, ack='auto')
+        print(f"Subscribed to ActiveMQ queue: {queue_name_two}")
+    elif current_payment_gateway_status['payment_gate_way_two'] == 'down':
+        try:
+            conn.unsubscribe(id=2)
+            print(f"Unsubscribed from ActiveMQ queue: {queue_name_two}")
+        except NameError:
+            print(f"Unknown subscription to ActiveMQ queue: {queue_name_two}")
+
+def process_payments(payment):
+    if current_payment_gateway_status['payment_gate_way_one'] == 'up' and payment.gateway == 'one':
+        #unproccesed_payment_one = Payment.query.filter(Payment.gateway == 'one', Payment.processed == False).all()
+        #for payment in unproccesed_payment_one:
+        payment.is_retry = True
+        response = requests.post(f"{hostOne}/execute_payment", json=payment_schema.dump(mapper_to_gateway(payment)))
+        if response.status_code == 200:
+            payment.processed = True
+            #db.session.add(payment)
+            #db.session.commit()
+            print('NOTIFY PAYMENT_GATEWAY_ONE PROCESSED -> {%s:%s}' % (payment.id, payment.value))
+    if current_payment_gateway_status['payment_gate_way_two'] == 'up' and payment.gateway == 'two':
+        #unproccesed_payment_two = Payment.query.filter(Payment.gateway == 'two', Payment.processed == False).all()
+        #for payment in unproccesed_payment_two:
+        payment.is_retry = True
+        response = requests.post(f"{hostTwo}/execute_payment", json=payment_schema.dump(mapper_to_gateway(payment)))
+        if response.status_code == 200:
+            payment.processed = True
+            #db.session.commit()
+            #db.session.commit()
+            print('NOTIFY PAYMENT_GATEWAY_TWO PROCESSED -> {%s:%s}' % (payment.id, payment.value))
 
 
 class TestPublishResource(Resource):
