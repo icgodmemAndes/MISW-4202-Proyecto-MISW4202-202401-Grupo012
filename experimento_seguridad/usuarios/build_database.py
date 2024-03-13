5647a798636ad014d9da9a60d04b5639294bbfd0
cristian.parada <cristian.parada@COC02Y30G9JGH7.local>
2024-03-12 23:12:48 -0500

    add seed for user and authorizator dbs

--- experimento_seguridad/usuarios/build_database.py
+++ experimento_seguridad/usuarios/build_database.py
@@ -9,7 +9,7 @@ faker = Faker()
 
 for i in range(10):
     user_name = faker.unique.name()
-    user = User(id=i, full_name=user_name, email=f"{user_name}@{faker.random.domain_name()}", plan=faker.random.choice([PlanType.BASIC.value, PlanType.PREMIUM.value]))
+    user = User(id=i, full_name=user_name, email=f"{user_name}@{faker.word()}.com", plan=faker.random.choice([PlanType.BASIC.value, PlanType.PREMIUM.value]))
     
     db.session.add(user)
     db.session.commit()
